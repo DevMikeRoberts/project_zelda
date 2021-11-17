1f27cbd705fd9a2931b4c7d300fff6a40647296d
Allytaih <91499514+Allytaih@users.noreply.github.com>
2021-11-17 04:01:58 -0500

    wk8

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Portal : Collideable   {
 
     public string nextScene;
     private Player player;
+    public LevelEnd EndScreen;
 
     protected override void OnCollide(Collider2D coll) {
 
@@ -14,7 +16,8 @@ public class Portal : Collideable   {
             //Teleport the player to next scene
             GameManager.instance.SaveState();
             player = GameObject.Find("Player").GetComponent<Player>();
-
+            //create level end menu here
+            EndScreen.CreateEM();
             player.endLevel = 1; // should initiate LoadNextLevel;
         }
 
